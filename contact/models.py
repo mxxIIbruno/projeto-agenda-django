48d06547f7ad3b92e8c569bc4918563883050a2d
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-03-25 13:17:54 -0300

    Model Meta options, verbose_name e verbose_name_plural no Django

--- contact/models.py
+++ contact/models.py
@@ -11,6 +11,10 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
