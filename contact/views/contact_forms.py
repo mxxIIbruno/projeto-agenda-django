4e8e710b4b52f543e829f127e8fd04b0e0228eba
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-04-01 00:05:30 -0300

    Usando request.method e request.POST para saber quando o formulário é postado / Criando um formulário dinâmico com forms.ModelForm do Django

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,53 @@
+from typing import Any
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django import forms
+from django.core.exceptions import ValidationError
+
 from contact.models import Contact
 
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = 'first_name', 'last_name', 'phone'
+
+    def clean(self) -> dict[str, Any]:
+        cleaned_data = self.cleaned_data
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de Erro',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de Erro',
+                code='invalid'
+            )
+        )
+        return super().clean()
+
+
 def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request=request,
+            template_name='contact/create.html',
+            context=context,
+        )
+
     context = {
-        ...: ...
+        'form': ContactForm()
     }
+
     return render(
         request=request,
         template_name='contact/create.html',
