687a6bf6e5e085d7728f16eadb0f895463ab6217
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-04-02 22:43:16 -0300

    Criando a relação entre contact e owner (request.user)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -42,7 +42,9 @@ def create(request):
 
 @login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.use
+    )
     form_action = reverse('contact:update', args=(contact_id, ))
 
     if request.method == 'POST':
@@ -54,7 +56,9 @@ def update(request, contact_id):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
 
         return render(
@@ -77,7 +81,9 @@ def update(request, contact_id):
 
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.use
+    )
     confirmation = request.POST.get('confirmation', 'no')
 
     if confirmation == 'yes':
